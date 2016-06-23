56301fbae5ac30602ae9473d87cb37ad3e5980f2
rems@elang <montrotyr@mfi.fr>
2016-06-23 17:22:59 +0000

    After installing lxml, update to parser xml properly

--- inprogress/configurations/read_xml_config.py
+++ inprogress/configurations/read_xml_config.py
@@ -1,9 +1,23 @@
-from BeautifulSoup import BeautifulSoup
+## pip install BeautifulSoup4 (needed for python3)
+#  https://www.crummy.com/software/BeautifulSoup/bs4/doc
+#
+# Also required a "pip install lxml"
+from bs4 import BeautifulSoup
 
-with open("config.xml") as f:
+with open("config_read.xml") as f:
     content = f.read()
 
-y = BeautifulSoup(content)
+#y = BeautifulSoup(content, "xml.parser")
+#y = BeautifulSoup(content, "lxml")
+
+## the only currently supported XML parser
+y = BeautifulSoup(content, "lxml-xml")
+#y = BeautifulSoup(content, "html.parser")
+
+
+## Access different elements of the xml 
 print(y.mysql.host.contents[0])
-for tag in y.other.preprocessing_queue:
+#for tag in y.other.preprocessing_queue:
+for tag in y.other:
+
     print(tag)
